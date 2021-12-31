fd75761cc64c4a7db9c9e98acada9979fb1cea86
Han Nguyen <hannv88@gmail.com>
2021-12-31 15:02:55 +0700

    remove order file

---
+++ Hub.BackgroundJob.Main/Models/EventBusConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hub.BackgroundJob.Main.Models
+{
+    public class EventBusConfiguration
+    {
+        public string EventBusConnection { get; set; }
+        public int EventBusRetryCount { get; set; }
+        public string BrokerName { get; set; }
+        public string SubscriptionClientName { get; set; }
+        public string EventBusUserName { get; set; }
+        public string EventBusPassword { get; set; }
+    }
+}
